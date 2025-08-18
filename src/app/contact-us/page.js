9f56f68bdebca7e63851d5a4f28f35ff2fb55bed
utsav <utsavdoris@gmail.com>
2025-08-18 10:05:26 +0530

    update

---
+++ src/app/contact-us/page.js
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return (
+        <main>
+            <h1>Contact Us</h1>
+            <p>This is the Contact page of the site.</p>
+        </main>
+    );
+}
