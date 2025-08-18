dd42eefdf8f6747b48ae6ed10d6a57129b3da0ef
utsav <utsavdoris@gmail.com>
2025-08-18 12:16:20 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,15 @@
-'use client';
 import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <main>
-          {/* <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about-us">About</Link>
-            <Link href="/contact-us">Contact</Link>
-          </nav> */}
-          {children}
-        </main>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about-us">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+        {children}
       </body>
     </html>
   );
