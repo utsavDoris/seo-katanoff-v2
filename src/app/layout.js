569652b32d83af1c51a017f1a6f8a4fc25950304
utsav <utsavdoris@gmail.com>
2025-08-18 11:36:57 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/about-us">About</Link>
-          <Link href="/contact-us">Contact</Link>
-        </nav>
-        {children}
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/about-us">About</Link>
+            <Link href="/contact-us">Contact</Link>
+          </nav>
+          {children}
+        </main>
       </body>
     </html>
   );
