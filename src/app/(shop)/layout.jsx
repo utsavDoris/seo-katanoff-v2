061ee298fd9d89a7de000bb577c1cbe1d7702baf
utsav <utsavdoris@gmail.com>
2025-08-21 14:31:20 +0530

    updte

--- src/app/(shop)/layout.jsx
+++ src/app/(shop)/layout.jsx
@@ -3,12 +3,12 @@
 import Footer from "@/components/layout/footer";
 // import Header from "@/components/layout/header";
 
-export default function ShopLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <div>
+    <main>
       {/* <Header /> */}
-      <main className="flex-1">{children}</main>
+      {children}
       <Footer />
-    </div>
+    </main>
   );
 }
