7a2950c35b13df271d1ffb97ffcaf552d2ec55cb
utsav <utsavdoris@gmail.com>
2025-08-21 17:27:25 +0530

    resolve

---
+++ src/app/(shop)/custom-jewelry-form/layout.js
@@ -0,0 +1,19 @@
+// import { generateMetadata } from "@/_utils/metaConfig";
+// import { WebsiteUrl } from "@/_helper";
+
+// const META_TITLE = "About Katanoff | Fine Jewelry & Unique Craftsmanship";
+// const META_DESCRIPTION =
+//   "At Katanoff, we create timeless jewelry with exceptional craftsmanship and elegant designs. Our passion for fine jewelry inspires every collection, offering clients lasting beauty and value.";
+// const META_KEYWORDS =
+//   "Katanoff, fine jewelry, handcrafted jewelry, timeless jewelry, unique craftsmanship, diamond jewelry, gold jewelry, custom jewelry, elegant accessories";
+// const CANONICAL_URL = `${WebsiteUrl}/about-us`;
+
+// export const metadata = generateMetadata({
+//   title: META_TITLE,
+//   description: META_DESCRIPTION,
+//   keywords: META_KEYWORDS,
+//   url: CANONICAL_URL,
+// });
+export default function CustomJewelryFormLayout({ children }) {
+  return <div>{children}</div>;
+}
