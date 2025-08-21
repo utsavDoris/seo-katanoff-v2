97b0e77f0f10410d8220c539b4008b1f189d2ca4
utsav <utsavdoris@gmail.com>
2025-08-21 15:30:26 +0530

    sdiasf

--- src/app/(shop)/layout.jsx
+++ src/app/(shop)/layout.jsx
@@ -1,10 +1,6 @@
 // import { Footer, Header } from "@/components/dynamiComponents";
 
-import dynamic from "next/dynamic.js";
-
-const Footer = dynamic(() => import("../../components/layout/footer"), {
-  ssr: false,
-});
+import Footer from "@/components/layout/footer";
 
 // import Footer from "@/components/layout/footer";
 // import Header from "@/components/layout/header";
