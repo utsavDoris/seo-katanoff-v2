f7ae3cc7bc38305de8064e33d981c7c0c9a9aafd
utsav <utsavdoris@gmail.com>
2025-08-22 09:55:26 +0530

    update

--- src/app/(shop)/layout.jsx
+++ src/app/(shop)/layout.jsx
@@ -1,12 +1,12 @@
 // import { Footer, Header } from "@/components/dynamiComponents";
-// import { Footer } from "@/components/dynamiComponents";
+import { Footer } from "@/components/dynamiComponents";
 
 export default function ShopLayout({ children }) {
   return (
     <main>
       {/* <Header /> */}
-      {children}
-      {/* <Footer /> */}
+      {/* {children} */}
+      <Footer />
     </main>
   );
 }
