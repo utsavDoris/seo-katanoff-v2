949439b67f971b100ad2e31481b37740107b4f14
utsav <utsavdoris@gmail.com>
2025-08-22 10:03:20 +0530

    update

--- src/app/(account)/order-history/page.jsx
+++ src/app/(account)/order-history/page.jsx
@@ -1,5 +1,6 @@
-import { OrderHistoryPage } from "@/components/dynamiComponents";
+// import { OrderHistoryPage } from "@/components/dynamiComponents";
 
 export default function OrderHistory() {
-  return <OrderHistoryPage />;
+  // return <OrderHistoryPage />;
+  return <h1>Order History</h1>;
 }
